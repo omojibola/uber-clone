d1e2139f2fcb15f387542d017d980c0e9667b655
omojibola <omojibolaorija@gmail.com>
2021-08-05 17:11:39 +0100

    created home page

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const MapScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Map Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
